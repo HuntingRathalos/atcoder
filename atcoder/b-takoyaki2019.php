e61ef3908f878613938c1f8d554ce6aab8cbce67
HuntingRathalos <huntingandrathalos@gmail.com>
2022-01-24 19:02:06 +0900

    b-takoyaki2019演習

---
+++ atcoder/b-takoyaki2019.php
@@ -0,0 +1,11 @@
+<?php
+fscanf(STDIN, "%d", $n);
+$d = explode(" ", trim(fgets(STDIN)));
+$ans = 0;
+for($i = 0; $i < $n; $i++) {
+    for($j = $i+1; $j < $n; $j++) {
+        $ans += $d[$i] * $d[$j];
+    }
+}
+
+echo $ans;
