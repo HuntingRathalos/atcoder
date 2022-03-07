2396d2a5c0fc662cd626b465ccbd0ec78f14a3b3
HuntingRathalos <huntingandrathalos@gmail.com>
2022-03-07 20:52:58 +0900

    b-YYMM演習修正

--- atcoder/b-YYMM.php
+++ atcoder/b-YYMM.php
@@ -3,30 +3,29 @@ fscanf(STDIN,"%s",$s);
 $char = '';
 $arr = [];
 
-for($i=0;$i<4;$i++){ // 全部で4桁と決まっているので4回ループ
-    $char = substr($s,$i,1); // substrで$sの１文字を抜く
-    array_push($arr,$char); //$arrに$charを追加する
+for($i=0;$i<4;$i++){
+    $char = substr($s,$i,1);
+    array_push($arr,$char);
 }
 
-$f_half = $arr[0].$arr[1]; //前半の二桁
-$s_half = $arr[2].$arr[3]; //後半の二桁
+$f_half = $arr[0].$arr[1];
+$s_half = $arr[2].$arr[3];
 
-$flg = 0; //判定用
+$flg = 0;
 
-//年は99あであり得るが、月は0~12なので月基準で判定
-if($f_half>0 && $f_half<=12){ // 0以上12以下なら前半は月と言える
+if($f_half>0 && $f_half<=12){
     $flg += 1;
 }
-if($s_half>0 && $s_half<=12){ // 0以上12以下なら後半は月と言える
+if($s_half>0 && $s_half<=12){
     $flg += 2;
 }
 
 if($flg == 0){
-    echo 'NA'; // 前半後半どちらも月とは言えない場合 (9999とか)
+    echo 'NA';
 } elseif ($flg == 1){
-    echo 'MMYY'; // 前半が月なら後半は年 で確定
+    echo 'MMYY';
 } elseif ($flg == 2){
-    echo 'YYMM'; // 後半が月なら前半は年 で確定
+    echo 'YYMM';
 } else {
-    echo 'AMBIGUOUS'; // どっちの可能性もある場合 (1111とか)
+    echo 'AMBIGUOUS'; 
 }
